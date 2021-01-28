629c47617360ddc5b2af69f1732cd8fc74adbd70
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-28 10:59:55 +0100

    Add first if conditions

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict';
 const betElement = document.querySelector('.js-myBet');
 const trialElement = document.querySelector('.js-trialBtn');
+const attemptElement = document.querySelector('.js-trialCounter');
+const noticeElement = document.querySelector('.js-notice');
+const messageElement = document.querySelector('.js-message');
+const attemptsNumber = attemptElement.innerHTML;
 const randomNumber = getRandomNumber(100);
 let trialCounter = 0;
 
@@ -12,7 +16,18 @@ function getRandomNumber(max) {
 
 function handleTrialButton (ev) {
     ev.preventDefault()
-    const myBet = trialElement.value;
+    trialCounter++;
+    // console.log(trialCounter);
     console.log('me han clicado');
+    const myBet = parseInt(betElement.value);
+    console.log(myBet);
+    if (myBet === randomNumber) {
+        noticeElement.classList.toggle('hidden');
+        messageElement.innerHTML = '¡¡¡Has ganado campeona!!!';
+    } else {
+        noticeElement.classList.toggle('hidden');
+        messageElement.innerHTML = 'Inténtalo de nuevo :)';
+    }
+
 }
 trialElement.addEventListener ('click', handleTrialButton);
