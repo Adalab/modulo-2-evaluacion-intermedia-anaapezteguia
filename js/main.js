be8b47e792ece847111771b247976884f8c065fb
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-28 11:12:22 +0100

    Add final conditionals

--- js/main.js
+++ js/main.js
@@ -18,16 +18,30 @@ function handleTrialButton (ev) {
     ev.preventDefault()
     trialCounter++;
     // console.log(trialCounter);
-    console.log('me han clicado');
+    // console.log('me han clicado');
     const myBet = parseInt(betElement.value);
     console.log(myBet);
-    if (myBet === randomNumber) {
+    if (myBet >100 || myBet <= 0) {
+        attemptElement.innerHTML = trialCounter;
+        noticeElement.classList.toggle('hidden');
+        messageElement.innerHTML = 'Tu número debe estar entre 1 y 100';
+    } else if (myBet === randomNumber) {
+        attemptElement.innerHTML = trialCounter;
         noticeElement.classList.toggle('hidden');
         messageElement.innerHTML = '¡¡¡Has ganado campeona!!!';
+    } else if (myBet > randomNumber) {
+        attemptElement.innerHTML = trialCounter;
+        noticeElement.classList.toggle('hidden');
+        messageElement.innerHTML = 'Tu número es demasiado alto';
+    } else if (myBet < randomNumber) {
+        attemptElement.innerHTML = trialCounter;
+        noticeElement.classList.toggle('hidden');
+        messageElement.innerHTML = 'Tu número es demasiado bajo';
     } else {
+        attemptElement.innerHTML = trialCounter;
         noticeElement.classList.toggle('hidden');
         messageElement.innerHTML = 'Inténtalo de nuevo :)';
     }
-
+    
 }
 trialElement.addEventListener ('click', handleTrialButton);
