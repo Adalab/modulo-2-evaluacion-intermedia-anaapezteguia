d93b848e2f5b377b0d727cf8e03c1123f37e0863
Ana Apezteguía <ana.apezteguia@gmail.com>
2021-01-28 10:30:19 +0100

    Update js classes in HTML and start JS functions, random and event listener

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+const betElement = document.querySelector('.js-myBet');
+const trialElement = document.querySelector('.js-trialBtn');
+const randomNumber = getRandomNumber(100);
+let trialCounter = 0;
+
+console.log(randomNumber);
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+
+function handleTrialButton (ev) {
+    ev.preventDefault()
+    const myBet = trialElement.value;
+    console.log('me han clicado');
+}
+trialElement.addEventListener ('click', handleTrialButton);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Módulo 2. Evaluación intermedia
+
+Este repo contiene la evaluación intermedia de Ana Apezteguía del bootcamp de Adalab :)
